2b0ba10fbf96379348a756983af4b08081df1f0d
Nhan Ho <human@nhanho.me>
2016-08-12 00:19:53 +0700

    Move GC to main

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@
 // If we have this line, we can make sure that globals bootstrap and game loop work.
 // http://support.screeps.com/hc/en-us/articles/204825672-New-main-loop-architecture
 
-import { loadProcessTable, run, storeProcessTable } from "./components/kernel/kernel";
+import { loadProcessTable, run, storeProcessTable, garbageCollection } from "./components/kernel/kernel";
 import P = require("./components/processes/room/starter");
 import colony = require("./components/processes/room/colony");
 import room_position = require("./prototypes/room-position");
@@ -16,18 +16,20 @@ import { startRoomMining } from "./components/cli/mining";
 import structure_prototype = require("./prototypes/structure");
 declare var global: any;
 import cli = require("./components/cli/wrapper");
+global.start = P.start;
+global.startRoom = colony.start;
+global.startRoomMining = startRoomMining;
+
 export function loop() {
     // This is executed every tick
     global.__ = cli;
     room_position();
     structure_prototype();
     loadProcessTable();
-    global.start = P.start;
-    global.startRoom = colony.start;
-    global.startRoomMining = startRoomMining;
+    garbageCollection();
     run();
     storeProcessTable();
 
     // We reload the process table, to make sure it's updated for our command line
-    loadProcessTable(); 
+    loadProcessTable();
 }
